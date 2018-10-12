c8ec3120e0740e25ab03e38da4cb1f12e5ef25f2
Vernon Williams <voxenn@gmail.com>
2018-10-11 21:39:20 -0500

    First commit

---
+++ src/main/java/com/example/demo/RelesApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@EnableOAuth2Sso
+@SpringBootApplication
+public class RelesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelesApplication.class, args);
+    }
+}
